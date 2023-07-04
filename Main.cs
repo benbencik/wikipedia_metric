6d136bd16b0c393df47513880137811447f67f47
cechovic <markocechovic@gmail.com>
2023-07-04 19:18:58 +0200

    feat(parser): initial setup

--- Main.cs
+++ Main.cs
@@ -1,10 +1,10 @@
-﻿namespace HelloWorld
+﻿namespace WikipediaMetric
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WikimediaParser.ParseFrom("Ahoj");
         }
     }
-} 
+}
---
+++ WikisourceParser.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WikipediaMetric
+{
+    class WikimediaParser
+    {
+        public static void ParseFrom(string test)
+        {
+            Console.WriteLine(test);
+        }
+    }
+}
