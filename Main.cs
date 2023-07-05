8d7a52fe8c4ff7564f64f4fe2e14d883dbd9b24c
cechovic <markocechovic@gmail.com>
2023-07-05 19:37:42 +0200

    fix(main): return value

--- Main.cs
+++ Main.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            WikimediaParser.ParseFrom("data/wikisource_dummy.txt");
-            // WikimediaParser.ParseFrom("data/enwiki-20230401-pages-articles-multistream1.xml-p1p41242");
+            var map = WikimediaParser.ParseFrom("data/wikisource_dummy.txt");
+            // var map = WikimediaParser.ParseFrom("data/enwiki-20230401-pages-articles-multistream1.xml-p1p41242");
         }
     }
 }
