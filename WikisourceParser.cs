78282c7db91220a4323da92374604c0c964f3860
cechovic <markocechovic@gmail.com>
2023-07-05 18:33:17 +0200

    refactor(parser): +add comments

--- WikisourceParser.cs
+++ WikisourceParser.cs
@@ -11,6 +11,82 @@ namespace WikipediaMetric
 {
     partial class WikimediaParser
     {
+        private static string _pageTag;
+        private static StringBuilder _pageBuffer;
+        // true if reading contents of (inside of) a page (<page> contents </page>)
+        private static bool _readingPage;
+
+        private static Regex _titleRegex;
+        private static Regex _textRegex;
+        private static Regex _linksRegex;
+
+        // Map page title with its links
+        private static Dictionary<string, IEnumerable<string>> _map;
+
+        static WikimediaParser()
+        {
+            _pageTag = "page>";
+            _pageBuffer = new();
+            _readingPage = false;
+
+            _titleRegex = titleRegex();
+            _textRegex = textRegex();
+            _linksRegex = linksRegex();
+
+            _map = new();
+        }
+
+        public static void ParseFrom(string path)
+        {
+            Console.WriteLine("Reading file: " + path);
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            foreach (string currentLine in GetLines(path))
+            {
+                // Detect start and end of a page
+                // We are detecting only `page>` since wikisource dumps are
+                // valid XML files so there is always only one opening and
+                // one corresponding closing tag
+                if (currentLine.Contains(_pageTag))
+                {
+                    ProcessPage();
+                    _readingPage = !_readingPage;
+                    continue;
+                }
+
+                if (_readingPage)
+                {
+                    // Read the page content to a buffer
+                    // Page content is between <page> and </page> tags
+                    _pageBuffer.Append(currentLine);
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+
+        private static void ProcessPage()
+        {
+            // We detected _pageTag and we are _readingPage so it must be
+            // the closing tag, hence we process the page in the _pageBuffer
+            if (_readingPage)
+            {
+                // Get page title
+                var title = _titleRegex.Matches(_pageBuffer.ToString())[0].Groups[1].Value;
+                // Get page text so we can extract links
+                var text = _textRegex.Matches(_pageBuffer.ToString())[0].Groups[1].Value;
+                // Extract links from the page text
+                var links = _linksRegex.Matches(text);
+
+                // Map links from the page to the corresponding title
+                var linksList = from link in links select link.Groups[1].Value;
+                _map.Add(title, linksList);
+
+                // Clear the buffer so we can read to again a new page
+                _pageBuffer.Clear();
+            }
+        }
+
         private static IEnumerable GetLines(string path)
         {
             try
@@ -23,62 +99,12 @@ namespace WikipediaMetric
                 throw;
             }
         }
-        public static void ParseFrom(string path)
-        {
-            Console.WriteLine("Reading file: " + path);
-
-            // TODO: Move to class level private variables
-            var pageTag = "page>";
-            var pageBuffer = new StringBuilder();
-            var readingPage = false;
-
-            Regex titleRegex = TitleRegex();
-            Regex textRegex = TextRegex();
-            Regex linksRegex = LinksRegex();
-
-            // Map page title with its links
-            var map = new Dictionary<string, IEnumerable<string>>();
-
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            foreach (string line in GetLines(path))
-            {
-                if (line.Contains(pageTag))
-                {
-                    // TODO: Refactor this section to functions, eg. put regex initialisations into some __init__
-                    // as well as the map variable, than ParseFrom will read lines and in this section
-                    // just processPage will be called which will have access to all class variables
-                    if (readingPage)
-                    {
-                        // Process the page
-                        var title = titleRegex.Matches(pageBuffer.ToString())[0].Groups[1].Value;
-                        var text = textRegex.Matches(pageBuffer.ToString())[0].Groups[1].Value;
-                        var links = linksRegex.Matches(text);
-
-                        // Map links from the page to the corresponding title
-                        var linksList = from link in links select link.Groups[1].Value;
-                        map.Add(title, linksList);
-
-                        // Clear the buffer so we can read to again a new page
-                        pageBuffer.Clear();
-                    }
-                    readingPage = !readingPage;
-                    continue;
-                }
-
-                if (readingPage)
-                {
-                    pageBuffer.Append(line);
-                }
-            }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.ElapsedMilliseconds);
-        }
 
         [GeneratedRegex(@"<title[^>]*>([^<]*)<\/title>", RegexOptions.IgnoreCase | RegexOptions.Compiled)]
-        private static partial Regex TitleRegex();
+        private static partial Regex titleRegex();
         [GeneratedRegex(@"<text[^>]*>([^<]*)<\/text>", RegexOptions.IgnoreCase | RegexOptions.Compiled)]
-        private static partial Regex TextRegex();
+        private static partial Regex textRegex();
         [GeneratedRegex(@"\[\[(?!File:)([^\[\]|]*)", RegexOptions.IgnoreCase | RegexOptions.Compiled)]
-        private static partial Regex LinksRegex();
+        private static partial Regex linksRegex();
     }
 }
