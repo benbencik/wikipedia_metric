41596b5107ea5b219a5df04c8c05b8f3d9f6d15a
cechovic <markocechovic@gmail.com>
2023-07-05 19:39:17 +0200

    fix(logger): implicit usings

--- Logger.cs
+++ Logger.cs
@@ -1,44 +1,51 @@
-public enum LogLevel
-{
-    Info,
-    Warning,
-    Error
-}
 
-public class Logger
+
+using System;
+
+namespace WikipediaMetric
 {
-    public void Log(LogLevel level, string message)
+    public enum LogLevel
     {
-        string logEntry = $"{DateTime.Now} - [{level}] - {message}";
+        Info,
+        Warning,
+        Error
+    }
 
-        switch (level)
+    public class Logger
+    {
+        public void Log(LogLevel level, string message)
         {
-            case LogLevel.Info:
-                Console.ForegroundColor = ConsoleColor.White; // Set color for Info logs
-                break;
-            case LogLevel.Warning:
-                Console.ForegroundColor = ConsoleColor.Yellow; // Set color for Warning logs
-                break;
-            case LogLevel.Error:
-                Console.ForegroundColor = ConsoleColor.Red; // Set color for Error logs
-                break;
+            string logEntry = $"{DateTime.Now} - [{level}] - {message}";
+
+            switch (level)
+            {
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.White; // Set color for Info logs
+                    break;
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow; // Set color for Warning logs
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Red; // Set color for Error logs
+                    break;
+            }
+            Console.WriteLine(logEntry);  // Log to console
+            Console.ResetColor(); // Reset console color
         }
-        Console.WriteLine(logEntry);  // Log to console
-        Console.ResetColor(); // Reset console color
-    }
 
-    public void LogInfo(string message)
-    {
-        Log(LogLevel.Info, message);
-    }
+        public void LogInfo(string message)
+        {
+            Log(LogLevel.Info, message);
+        }
 
-    public void LogWarning(string message)
-    {
-        Log(LogLevel.Warning, message);
-    }
+        public void LogWarning(string message)
+        {
+            Log(LogLevel.Warning, message);
+        }
 
-    public void LogError(string message)
-    {
-        Log(LogLevel.Error, message);
+        public void LogError(string message)
+        {
+            Log(LogLevel.Error, message);
+        }
     }
 }
