50a9375193f73bbcbf231dd3775fd43c7d59295a
cechovic <markocechovic@gmail.com>
2023-07-06 12:25:34 +0200

    feat(json): initial implementation of ToFile

--- Json.cs
+++ Json.cs
@@ -1,18 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace WikipediaMetric
 {
 
-    static class Json
+    static class JsonManager
     {
-        private static Logger _logger;
-        static Json()
+        private static readonly Logger _logger;
+
+        static JsonManager()
         {
-            _logger = new Logger(nameof(Json));
+            _logger = new Logger(nameof(JsonManager));
         }
         // Saves TMap of page titles and corresponding links to a json file
         public static void ToFile(TMap titleLinksMap, string filePath)
         {
             _logger.Info("Saving TMap to the file: " + filePath);
-            
+
+            using var file = FileManager.GetStreamWriter(filePath);
+
+            file.WriteLine("[");
+
+            file.Write(FormatPairs(titleLinksMap));
+
+            file.WriteLine("]");
         }
 
         // Loads Tmap of page titles and corresponding links from a json file
@@ -21,6 +33,42 @@ namespace WikipediaMetric
             _logger.Info("Loading TMap from the file: " + filePath);
 
             return new TMap();
+        }
+
+        private static string FormatPairs(TMap titleLinksMap)
+        {
+            var formatted = new StringBuilder();
+            foreach (var pair in titleLinksMap)
+            {
+                formatted.AppendLine("	{");
+
+                formatted.AppendLine($@"		""title"": ""{pair.Key}"",");
+                formatted.AppendLine($@"		""links"": [""{FormatLinks(pair.Value)}""]");
+
+                formatted.AppendLine("	},");
+            }
+
+            // Remove trailing comma
+            formatted = formatted.Remove(formatted.Length - 3, 3);
+            formatted.Append(Environment.NewLine);
+
+            return formatted.ToString();
+        }
+
+        private static string FormatLinks(IEnumerable<string> links)
+        {
+            var formatted = new StringBuilder();
+
+            foreach (var link in links)
+            {
+                formatted.Append(link);
+                formatted.Append(',');
+            }
+
+            // Remove trailing comma
+            formatted = formatted.Remove(formatted.Length - 1, 1);
+
+            return formatted.ToString();
         }
     }
 }
--- Main.cs
+++ Main.cs
@@ -10,6 +10,9 @@ namespace WikipediaMetric
 
             var map = WikimediaParser.ParseFrom("data/wikisource_dummy.txt");
             // var map = WikimediaParser.ParseFrom("data/enwiki-20230401-pages-articles-multistream1.xml-p1p41242");
+
+            JsonManager.ToFile(map, "data/test.json");
         }
     }
 }
+
