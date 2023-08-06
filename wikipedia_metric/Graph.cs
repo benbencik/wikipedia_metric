322d0c90793f56e5f2eef93a5e85a59c8e0275e1
benbencik <bencik42@gmail.com>
2023-08-06 10:52:41 +0200

    moved search into graph class

--- wikipedia_metric/Graph.cs
+++ wikipedia_metric/Graph.cs
@@ -183,6 +183,47 @@ namespace wikipedia_metric
             return searchResults;
         }
 
+        static List<string> ReconstructPath(string node, Dictionary<string, string> previous)
+        {
+            // reconstruct path by following predecessors of each node
+            List<string> path = new();
+            while (previous.ContainsKey(node))
+            {
+                path.Add(node);
+                node = previous[node];
+            }
+            path.Add(node);
+            path.Reverse();
+            return path;
+        }
+
+        public List<string> NaiveFindPath(string start, string end)
+        {
+            // implementation of uninformed search algorithm
+            Dictionary<string, int> distances = new();
+            Dictionary<string, string> previous = new();
+            Queue<string> queue = new();
+            queue.Enqueue(start);
+            distances.Add(start, 0);
+            
+            while (queue.Count > 0)
+            {
+                string current = queue.Dequeue();
+                if (current == end) { break; }
+
+                foreach (string neighbor in GetNeighbors(current))
+                {
+                    if (!distances.ContainsKey(neighbor))
+                    {
+                        distances.Add(neighbor, distances[current] + 1);
+                        previous.Add(neighbor, current);
+                        queue.Enqueue(neighbor);
+                    }
+                }
+            }
+            return ReconstructPath(end, previous);
+        }
     }
+
 
 }
